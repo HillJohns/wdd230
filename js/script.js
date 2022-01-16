0b4d999c85b54fb436b51f540d18f7433830a620
HillJohns <hillarytai11@gmail.com>
2022-01-15 19:24:50 -1000

    w01 fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-const options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long",
-  year: "numeric",
-  hour: "numeric",
-  minute: "numeric"
-};
-document.getElementById("currentdate").textContent =
-  new Date().toLocaleDateString("en-US", options);
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = document.lastModified;
