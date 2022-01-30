5787488fcaa0431152ec26628ae0a6685146f78d
HillJohns <hillarytai11@gmail.com>
2022-01-29 23:15:00 -0700

    moved images

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,6 +1,17 @@
+const datefieldUK = document.querySelector("date");
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(
+  now);
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+function toggleMenu() {
+  document.getElementById("navButton").classList.toggle("open");
+  document.getElementById("hamburger").classList.toggle("open");
+}
+const x = document.getElementById('hamburger')
+x.onclick = toggleMenu;
+
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = document.lastModified;
 
-function toggleMenu() {
-  document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
+
