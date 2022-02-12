c15bdeac9b8e1bd36a49ae2853ed981b74467d7d
HillJohns <hillarytai11@gmail.com>
2022-02-12 16:55:10 -0700

    Fixed hamburger menu to be verticle

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,9 +1,11 @@
+// Date
 const datefieldUK = document.querySelector("#date");
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(
   now);
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
+//Menu
 function toggleMenu() {
   document.getElementById("navButton").classList.toggle("open");
   document.getElementById("hamburger").classList.toggle("open");
@@ -11,9 +13,11 @@ function toggleMenu() {
 const x = document.getElementById('hamburger')
 x.onclick = toggleMenu;
 
+//Last Mod
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = document.lastModified;
 
+//Meeting Banner
 function displayBanner() {
   const weekday = now.getDay();
   const banner = document.querySelector('.banner');
