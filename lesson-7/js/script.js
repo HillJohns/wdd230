53e07b42b06eeed902c2417f7a5fd4c915968993
HillJohns <hillarytai11@gmail.com>
2022-02-17 23:25:16 -0700

    img sizes

--- lesson-7/js/script.js
+++ lesson-7/js/script.js
@@ -1,10 +1,34 @@
+
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-const loadImages = (image) => {
-  image.setAttribute('src', image.getAttribute('data-src'));
-  image.onload = () => {
-    image.removeAttribute('data-src');
-  };
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: '0px 0px -00px 0px'
+}
+
+const loadImages = (img) => {
+    img.setAttribute('src', img.getAttribute('data-src'));
+    img.onload = () => {
+        img.removeAttribute('data-src');
+    };
 };
-imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
+
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                loadImages(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    imagesToLoad.forEach((image) => {
+        observer.observe(image);
+    });
+} else {
+    imagesToLoad.forEach((image) => {
+        loadImages(image);
+    });
+}
+
